f9910a18461136ccfa1c1eb07fcd1ffb9d66cdc9
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-04-21 19:39:29 +0000

    Add for statement

--- nestEgg2.java
+++ nestEgg2.java
@@ -13,9 +13,6 @@ public class nestEgg2
   save = Integer.parseInt(args[1]);//Inputs percentage of your salary put into your retirement fund
   nretirement = Integer.parseInt(args[2]);//Inputs number of remaining years to retirement
   growthRate = Double.parseDouble(args[3]);//Inputs growth rate
-  double F1 = salary * save * 0.01;//Calculates your retirement fund for the fi$
-  double F2 = F1 * (1+0.01 * growthRate) + salary * save * 0.01;//Calculates yo$
-  double F3 = F2 * (1+0.01 * growthRate) + salary * save * 0.01;//Calculates yo$
 
   Scanner sf = new Scanner(new File("./growthRate.txt"));
   int maxIndx = -1;
@@ -27,9 +24,14 @@ public class nestEgg2
   }
   sf.close();
 
-  System.out.println("End of year 1:" + F1);
-  System.out.println("End of year 2:" + F2);
-  System.out.println("End of year 3:" + F3);
+  double G = 0;
+  for (int year = 0; year < nretirement; year++)
+  {
+   
+
+  System.out.println("End of year 1:" + F1 + text[0]);
+  System.out.println("End of year 2:" + F2 + text[1]);
+  System.out.println("End of year 3:" + F3 + text[2]);
  }
 }
 
