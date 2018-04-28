9a8b90ad71920eb5859650bfa26b616411fb368c
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-04-28 17:26:19 +0000

    Fix java floating decimal error

--- nestEgg2.java
+++ nestEgg2.java
@@ -1,41 +1,19 @@
-//Calculates your retirement fund each year
 import java.io.*;
 import java.util.*;
+
 import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
-public class nestEgg2
-{
- public static void main(String args[]) throws IOException
- {
-  double salary;
-  int save;
-  int nretirement;
-  double growthRate; 
-  salary = Double.parseDouble(args[0]);//Inputs your salary
-  save = Integer.parseInt(args[1]);//Inputs percentage of your salary put into your retirement fund
-  nretirement = Integer.parseInt(args[2]);//Inputs number of remaining years to retirement
-  growthRate = Double.parseDouble(args[3]);//Inputs growth rate
 
-  FileReader fr = new FileReader("growthRate.txt");//Reads txt file
-  BufferedReader textReader = new BufferedReader(fr);
-  double GrowthRates[] = new double[100];
-  for (int i = 1; i <= 100; i++)
-  {
-	GrowthRates[i] = Double.parseDouble(textReader.readLine());//Changes txt file to a double
+public class nestEgg2 {
+  public static void main(String args[]) throws IOException {
+    double data;
+    FileReader fr = new FileReader("growthRate.txt");
+    BufferedReader textReader = new BufferedReader(fr);
+			
+    for (int i = 1; i <= 60; i++){
+      data = Double.parseDouble(textReader.readLine());
+      System.out.println(data);
+    }
   }
-
-  double G = 0;
-  double F = 0;
-  int year;
-  for (year = 0; year < nretirement; year++)
-  {
-   F = G  * (1 + 0.01 * GrowthRates[year]) + salary * save * 0.01; //Calculates the value of this year
-   G = F;
-  }
-
-  System.out.println(year + F + GrowthRates[year]);
- }
 }
-
-
