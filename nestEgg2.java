e3ff1d2f21a9a160cad3adb3ac528ed1a7c135a0
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-04-28 17:38:52 +0000

    Add three command line arguments

--- nestEgg2.java
+++ nestEgg2.java
@@ -6,17 +6,22 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 
 public class nestEgg2 {
-  public static void main(String args[]) throws IOException {
+  public static void main(String args[]) throws IOException
+   {
     double salary;
     int save;
     int nretirement;
     double growthRate;
-    FileReader fr = new FileReader("growthRate.txt");
+    salary = Double.parseDouble(args[0]);//Inputs your salary
+    save = Integer.parseInt(args[1]);//Inputs percentage of your salary put into your retirement fund
+    nretirement = Integer.parseInt(args[2]);//Inputs number of remaining years to retirement
+
+    FileReader fr = new FileReader("growthRate.txt");//Reads txt file
     BufferedReader textReader = new BufferedReader(fr);
 			
     for (int i = 1; i <= 60; i++){
-      data = Double.parseDouble(textReader.readLine());
-      System.out.println(data);
+      growthRate = Double.parseDouble(textReader.readLine());
+      System.out.println(growthRate);
     }
   }
 }
