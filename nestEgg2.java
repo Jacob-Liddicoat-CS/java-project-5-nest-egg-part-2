cc0f52c365a8bdaa8ff2ba77884288d29342571b
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-04-28 17:53:06 +0000

    Add double array and for statement

--- nestEgg2.java
+++ nestEgg2.java
@@ -1,27 +1,29 @@
+//Calculates your retirement fund each year
 import java.io.*;
 import java.util.*;
-
 import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
+public class nestEgg2
+{
+ public static void main(String args[]) throws IOException
+ {
+  double salary;
+  int save;
+  int nretirement;
+  double growthRate;
+  salary = Double.parseDouble(args[0]);//Inputs your salary
+  save = Integer.parseInt(args[1]);//Inputs percentage of your salary put into your retirement fund
+  nretirement = Integer.parseInt(args[2]);//Inputs number of remaining years to retirement
 
-public class nestEgg2 {
-  public static void main(String args[]) throws IOException
-   {
-    double salary;
-    int save;
-    int nretirement;
-    double growthRate;
-    salary = Double.parseDouble(args[0]);//Inputs your salary
-    save = Integer.parseInt(args[1]);//Inputs percentage of your salary put into your retirement fund
-    nretirement = Integer.parseInt(args[2]);//Inputs number of remaining years to retirement
-
-    FileReader fr = new FileReader("growthRate.txt");//Reads txt file
-    BufferedReader textReader = new BufferedReader(fr);
-			
-    for (int i = 1; i <= 60; i++){
-      growthRate = Double.parseDouble(textReader.readLine());
-      System.out.println(growthRate);
-    }
+  FileReader fr = new FileReader("growthRate.txt");//Reads txt file
+  BufferedReader textReader = new BufferedReader(fr);
+  double GrowthRates[] = new double[60];
+  for (int i = 1; i <= 60; i++)
+  {
+	growthRate = Double.parseDouble(textReader.readLine());//Changes txt file to a double
   }
+   
+      System.out.println(growthRate);
+ }
 }
